10a95c12e35463503803926c98c8bf788ea06f37
aedifex <aedifex@gmail.com>
2026-03-22 09:06:00 -0700

    Update live env to v2

--- envs/dev/ec2/main.tf
+++ envs/dev/ec2/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source = "git::https://github.com/aedifex/harness-iacm-terraform-modules.git//modules/ec2?ref=main"
+  source = "git::https://github.com/aedifex/harness-iacm-terraform-modules.git//modules/ec2?ref=v2.0.0"
   name   = "Galacticus"
 }
 
