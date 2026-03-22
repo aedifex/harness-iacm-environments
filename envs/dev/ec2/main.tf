325d7fddc5c5f2f1f2267f8156aa88d3dce8b181
aedifex <aedifex@gmail.com>
2026-03-22 14:03:49 -0700

    Update module version

--- envs/dev/ec2/main.tf
+++ envs/dev/ec2/main.tf
@@ -1,6 +1,7 @@
 module "ec2" {
-  source = "git::https://github.com/aedifex/harness-iacm-terraform-modules.git//modules/ec2?ref=v2.0.0"
-  name   = "Galacticus"
+  source  = "git::https://github.com/aedifex/harness-iacm-terraform-modules.git//modules/ec2?ref=v2.0.0"
+  name    = "Galacticus"
+  version = "3.4.4"
 }
 
 output "instance_public_ip" {
