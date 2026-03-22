7fb749a3301a01d62906bbb1214965c67ae55276
aedifex <aedifex@gmail.com>
2026-03-21 19:43:25 -0700

    Update output variables

--- envs/dev/ec2/main.tf
+++ envs/dev/ec2/main.tf
@@ -2,3 +2,7 @@ module "ec2" {
   source = "git::https://github.com/aedifex/harness-iacm-terraform-modules.git//modules/ec2?ref=main"
   name   = "Galacticus"
 }
+
+output "instance_public_ip" {
+  value = module.ec2.instance_public_ip
+}
